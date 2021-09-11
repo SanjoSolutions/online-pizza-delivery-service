ecedb3358f20732fc47f47a906c4cee95c58ffe2
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-11 15:23:25 +0200

    Extension of generate_images.js to support multiple sizes
    Also fixes script.
    Also sets Node.js package type to "module".

---
+++ generate_images.js
@@ -0,0 +1,24 @@
+import sharp from 'sharp'
+import { generateImagePathForDimensions } from './src/generateImagePathForDimensions.js'
+
+const images = [
+  'public/images/kitchen_restaurant.jpg',
+  'public/images/wasser_restaurant.jpg'
+]
+
+const sizes = [
+  {width: 514, height: 289}
+]
+
+async function generateImages(imagePath) {
+  const image = sharp(imagePath)
+  await Promise.all(sizes.map(generateImage.bind(null, imagePath, image)))
+}
+
+async function generateImage(imagePath, image, {width, height}) {
+  await image
+    .resize(width, height)
+    .toFile(generateImagePathForDimensions(imagePath, width, height))
+}
+
+await Promise.all(images.map(generateImages))
